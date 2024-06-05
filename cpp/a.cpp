907e8db5e86dfb72c8c6f5790658ad0748abfe79
developomp <developomp@protonmail.com>
2024-06-06 00:18:52 +0900

    feat: basic fast io for C++

--- cpp/a.cpp
+++ cpp/a.cpp
@@ -4,9 +4,29 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC optimization("unroll-loops")
 
+#define N 3
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello, C++!\n";
+    // faster IO
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int sum = 0;
+    int nums[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> nums[i];
+        sum += nums[i];
+
+        cout << nums[i];
+
+        if (i < N - 1)
+            cout << " + ";
+    }
+
+    cout << " = " << sum << "\n";
 }
